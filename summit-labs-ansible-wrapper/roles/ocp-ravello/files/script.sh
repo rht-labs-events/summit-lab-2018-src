adf4bdf2f5f476dca9eb7f6f6689a882d814e7ef
Mangirdas <m.judeikis@gmail.com>
2018-04-22 19:31:21 +0100

    update after test

--- summit-labs-ansible-wrapper/roles/ocp-ravello/files/script.sh
+++ summit-labs-ansible-wrapper/roles/ocp-ravello/files/script.sh
@@ -2,4 +2,3 @@
 
 ansible-playbook --inventory-file=/usr/share/ansible/summit/inventory/ /usr/share/ansible/summit/playbooks/ocp-pre-ravello.yml
 ansible-playbook --inventory-file=/usr/share/ansible/summit/inventory/ /usr/share/ansible/summit/roles/applier/playbooks/openshift-cluster-seed.yml
-ansible-playbook --inventory-file=/usr/share/ansible/summit/inventory/ /usr/share/ansible/summit/playbooks/ocp-skydive.yml
